26a185dbe1ee0b2cb0c8b4ed997cd6674540f7fb
YosefAlfredo15 <82925912+YosefAlfredo15@users.noreply.github.com>
2024-05-29 23:16:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,37 @@
 import streamlit as st
+from PIL import Image
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+import tempfile
+import matplotlib.pyplot as plt
 
 # Fungsi untuk memuat dan menampilkan gambar serta melakukan prediksi
 def load_model():
     model = tf.keras.models.load_model('emotion_model.h5')
     return model
 
-def predict_emotion(model, img_path):
-    img = image.load_img(img_path, target_size=(48, 48))
-    img_array = image.img_to_array(img)
+def predict_emotion(model, img):
+    img_array = np.array(img.resize((48, 48)).convert('L'))  # Convert ke skala abu-abu dan resize gambar
     img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0
+    img_array = np.expand_dims(img_array, axis=-1)  # Tambahkan dimensi kedua dan keempat
+    img_array = img_array / 255.0  # Normalisasi
     prediction = model.predict(img_array)
     emotion_labels = ['Angry', 'Happy', 'Sad']
     predicted_class = np.argmax(prediction)
     return emotion_labels[predicted_class]
 
-def display_image_with_prediction(model, img_path):
-    img = image.load_img(img_path, target_size=(224, 224), interpolation='lanczos')
-    img_array = image.img_to_array(img)
-    img_array = img_array * 0.8
+
+def display_image_with_prediction(model, img):
+    resized_img = img.resize((224, 224), Image.LANCZOS)
+    img_array = np.array(resized_img) * 0.8
     img_array = np.clip(img_array, 0, 255)
     
     plt.imshow(img_array.astype(np.uint8))
     plt.axis('off')
     st.pyplot(plt)
 
-    predicted_emotion = predict_emotion(model, img_path)
+    predicted_emotion = predict_emotion(model, img)
     st.write(f'Hasil Prediksi: {predicted_emotion}')
 
 # Streamlit app
@@ -42,11 +41,17 @@ st.title("Emotion Detector")
 uploaded_file = st.file_uploader("Upload an image file", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    img_path = f'temp/{uploaded_file.name}'
-    with open(img_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file.write(uploaded_file.getvalue())
+        img_path = temp_file.name
     
+    img = Image.open(uploaded_file)
+    st.image(img, caption='Uploaded Image', use_column_width=True)
+    
+    # Sekarang Anda dapat memproses file gambar tanpa perlu menyimpannya ke direktori permanen
+    # Memuat model
     model = load_model()
-    display_image_with_prediction(model, img_path)
+    # Menampilkan gambar dengan prediksi emosi
+    display_image_with_prediction(model, img)
 else:
     st.write("Please upload an image file.")
