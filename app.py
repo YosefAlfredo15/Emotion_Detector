5d1828431836506dd45af035606f95701cd691ee
YosefAlfredo15 <82925912+YosefAlfredo15@users.noreply.github.com>
2024-05-29 21:47:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
 
 # Streamlit app
 st.title("Emotion Detector")
